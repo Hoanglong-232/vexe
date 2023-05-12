b5d844de9cf6ec04bd11919523a6234e81f4d38a
Your Name <you@example.com>
2023-05-12 17:10:33 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <components1></components1>
+  </div>
+</template>
+
+<script>
+import Ticket from './components/Ticket.vue'
+export default {
+  data () {
+    return {
+    
+    }
+  },
+  components: {
+    components1: Ticket
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+</style>
---
+++ src/components/Ticket.vue
@@ -0,0 +1,250 @@
+<template>
+   
+        <div class="box">
+            <div>
+                <strong>TUYẾN 1</strong>
+                <a-card aria-placeholder="TUYẾN 1" style="width: 585px ; border-radius: 30px; display: flex; justify-content: center; align-items: center; margin: 10px; background-color: #e1e1e1;">
+                <h5>QUÁN HOÁ ⇌ ĐÀ NẴNG
+                  </h5>
+                  <h6>500.000Đ</h6>
+                  <div class="desciption">
+                      <p>Loại xe:
+                        <strong>Xe 34 Phòng VIP</strong>
+                      </p>
+                      <p>Quảng đường di chuyển:
+                        <strong>720km</strong>
+                      </p>
+                      <p>Thời gian di chuyển:
+                        <strong>14 giờ</strong>
+                      </p>
+                      <p>Xuất bến Quan Hoá:
+                        <strong>15h00</strong>
+                      </p>
+                      <p>Xuất bến Đà Nẵng về:
+                        <strong>17h15’</strong>
+                      </p>
+                      <p>Lộ trình: Từ Quan Hóa đi Lang Chánh - Ngọc Lặc - Mục Sơn - QL47 - BX Phía Bắc - BX TT TP Đà Nẵng và ngược lại </p>
+                  </div>
+                </a-card>
+            </div>
+
+           
+            <div>
+                <strong>TUYẾN 2</strong>
+                <a-card style="width: 585px ; border-radius: 30px; margin: 10px; background-color: #e1e1e1;">
+                  <h5>CẨM THUỶ ⇌ ĐÀ NẴNG
+                  </h5>
+                  <h6>450.000Đ</h6>
+                  <div class="desciption">
+                      <p>Loại xe:
+                        <strong>Xe 34 Phòng VIP</strong>
+                      </p>
+                      <p>Quảng đường di chuyển:
+                        <strong>720km</strong>
+                      </p>
+                      <p>Thời gian di chuyển:
+                        <strong>14 giờ</strong>
+                      </p>
+                      <p>Xuất bến Quan Hoá:
+                        <strong>15h00</strong>
+                      </p>
+                      <p>Xuất bến Đà Nẵng về:
+                        <strong>17h15’</strong>
+                      </p>
+                      <p>Lộ trình: Từ Quan Hóa đi Lang Chánh - Ngọc Lặc - Mục Sơn - QL47 - BX Phía Bắc - BX TT TP Đà Nẵng và ngược lại </p>
+                  </div>
+                </a-card>
+            </div>
+
+           
+          <div>
+            <strong>TUYẾN 3</strong>
+                <a-card style="width: 585px ; border-radius: 30px; display: flex; justify-content: center; align-items: center; margin: 10px; background-color: #e1e1e1;">
+                  <h5>QUÁN HOÁ ⇌ ĐÀ NẴNG
+                  </h5>
+                  <h6>500.000Đ</h6>
+                  <div class="desciption">
+                      <p>Loại xe:
+                        <strong>Xe 34 Phòng VIP</strong>
+                      </p>
+                      <p>Quảng đường di chuyển:
+                        <strong>720km</strong>
+                      </p>
+                      <p>Thời gian di chuyển:
+                        <strong>14 giờ</strong>
+                      </p>
+                      <p>Xuất bến Quan Hoá:
+                        <strong>15h00</strong>
+                      </p>
+                      <p>Xuất bến Đà Nẵng về:
+                        <strong>17h15’</strong>
+                      </p>
+                      <p>Lộ trình: Từ Quan Hóa đi Lang Chánh - Ngọc Lặc - Mục Sơn - QL47 - BX Phía Bắc - BX TT TP Đà Nẵng và ngược lại </p>
+                  </div>
+                </a-card>
+          </div>
+
+       
+           <div>
+            <strong>TUYẾN 4</strong>
+                <a-card style="width: 585px ; border-radius: 30px; display: flex; justify-content: center; align-items: center; margin: 10px; background-color: #e1e1e1;">
+                  <h5>QUÁN HOÁ ⇌ ĐÀ NẴNG
+                  </h5>
+                  <h6>500.000Đ</h6>
+                  <div class="desciption">
+                      <p>Loại xe:
+                        <strong>Xe 34 Phòng VIP</strong>
+                      </p>
+                      <p>Quảng đường di chuyển:
+                        <strong>720km</strong>
+                      </p>
+                      <p>Thời gian di chuyển:
+                        <strong>14 giờ</strong>
+                      </p>
+                      <p>Xuất bến Quan Hoá:
+                        <strong>15h00</strong>
+                      </p>
+                      <p>Xuất bến Đà Nẵng về:
+                        <strong>17h15’</strong>
+                      </p>
+                      <p>Lộ trình: Từ Quan Hóa đi Lang Chánh - Ngọc Lặc - Mục Sơn - QL47 - BX Phía Bắc - BX TT TP Đà Nẵng và ngược lại </p>
+                  </div>
+                </a-card>
+    
+           </div>
+         
+            <div>
+                <strong>TUYẾN 5</strong>
+                <a-card style="width: 585px ; border-radius: 30px; display: flex; justify-content: center; align-items: center; margin: 10px; background-color: #e1e1e1;">
+                  <h5>QUÁN HOÁ ⇌ ĐÀ NẴNG
+                  </h5>
+                  <h6>500.000Đ</h6>
+                  <div class="desciption">
+                      <p>Loại xe:
+                        <strong>Xe 34 Phòng VIP</strong>
+                      </p>
+                      <p>Quảng đường di chuyển:
+                        <strong>720km</strong>
+                      </p>
+                      <p>Thời gian di chuyển:
+                        <strong>14 giờ</strong>
+                      </p>
+                      <p>Xuất bến Quan Hoá:
+                        <strong>15h00</strong>
+                      </p>
+                      <p>Xuất bến Đà Nẵng về:
+                        <strong>17h15’</strong>
+                      </p>
+                      <p>Lộ trình: Từ Quan Hóa đi Lang Chánh - Ngọc Lặc - Mục Sơn - QL47 - BX Phía Bắc - BX TT TP Đà Nẵng và ngược lại </p>
+                  </div>
+                </a-card>
+            </div>
+
+            <div>
+                <strong>TUYẾN 6</strong>
+                <a-card style="width: 585px ; border-radius: 30px; display: flex; justify-content: center; align-items: center; margin: 10px; background-color: #e1e1e1;">
+                  <h5>QUÁN HOÁ ⇌ ĐÀ NẴNG
+                  </h5>
+                  <h6>500.000Đ</h6>
+                  <div class="desciption">
+                      <p>Loại xe:
+                        <strong>Xe 34 Phòng VIP</strong>
+                      </p>
+                      <p>Quảng đường di chuyển:
+                        <strong>720km</strong>
+                      </p>
+                      <p>Thời gian di chuyển:
+                        <strong>14 giờ</strong>
+                      </p>
+                      <p>Xuất bến Quan Hoá:
+                        <strong>15h00</strong>
+                      </p>
+                      <p>Xuất bến Đà Nẵng về:
+                        <strong>17h15’</strong>
+                      </p>
+                      <p>Lộ trình: Từ Quan Hóa đi Lang Chánh - Ngọc Lặc - Mục Sơn - QL47 - BX Phía Bắc - BX TT TP Đà Nẵng và ngược lại </p>
+                  </div>
+                </a-card>
+            </div>
+        </div>
+  </template>
+  
+  
+  <script>
+import 'ant-design-vue/dist/antd.css';
+
+  export default {
+  }
+  </script>
+  <style scoped>
+    .box {
+        position: relative;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        /* display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(400px, 1fr)); */
+    }
+
+    strong {
+        font-weight: bolder;
+        margin-left: 12px;
+        font-size: 24px;
+    }
+
+    h4 {
+        font-size: 21px;
+        font-weight: 700;
+    }
+
+    h5 {
+        font-weight: bolder;
+        font-size: 18px;
+        background-color: #ffd401;
+        padding: 22px 20px;
+        margin-bottom: -55px;
+        margin-top: -24px;
+        margin-right: -24px;
+        margin-left: -24px;
+        border-radius: 99px;
+        font-weight: 500;
+        z-index: 1;
+        position: relative;
+        top: -7px;
+    }
+
+    h6 {
+        font-size: 30px;
+        color: #a81e22;
+        position: absolute;
+        display: inline-block;
+        right: 2px;
+        width: unset;
+        opacity: 1;
+        background: #fff;
+        border-radius: 99px;
+        top: -5px;
+        padding: 11px 16px;
+        margin: 0;
+        z-index: 2;
+    }
+
+    .desciption {
+        padding-top: 60px;
+    }
+
+    p {
+        font-weight: 500;
+        font-size: 16px;
+        display: flex;
+        justify-content: space-between;
+        border-top: 1px solid #fff;
+        margin: auto;
+        padding-top: 0.5em;
+        padding-bottom: 0.5em;
+    }
+
+    p:first-child {
+        border: none;
+    }
+  </style>  
